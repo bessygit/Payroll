c2ff1fe7834dfe5306693d8280b3310b471419c9
HP <HP@DESKTOP-3ADJN5A>
2023-07-21 04:07:11 +0100

    commit2

--- Payrollapp/src/main/java/com/obsqura/utilities/WaitUtility.java
+++ Payrollapp/src/main/java/com/obsqura/utilities/WaitUtility.java
@@ -17,6 +17,9 @@ public class WaitUtility {
 		this.driver = driver;
 
 	}
+	public void IW_ImplicitWait(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
 
 	public void waitforVisibilityofElement(WebElement ele) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -25,6 +28,10 @@ public class WaitUtility {
 	public void waitforElementtobeClickable(WebElement ele1) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.elementToBeClickable(ele1));
+	}
+	public void waitforPresenceofElement(By path,WebDriver driver) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(path));
 	}
 	
 	
